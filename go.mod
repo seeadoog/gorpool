6e988cc76542cf92d955aa3cb2190f9a973e7200
sjliu7 <1581503100@qq.com>
2021-03-25 17:33:37 +0800

    fix rps bug

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
 module github.com/seeadoog/gorpool
 
 go 1.14
+
--- rpool_test.go
+++ rpool_test.go
@@ -8,9 +8,9 @@ import (
 )
 
 func BenchmarkNewRPool(b *testing.B) {
-	p:=NewRPool()
+	p := NewRPool()
 	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next(){
+		for pb.Next() {
 			p.Execute(func() {
 
 			})
@@ -19,35 +19,29 @@ func BenchmarkNewRPool(b *testing.B) {
 
 }
 
-
 func BenchmarkNewRPool2(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next(){
-			go func() {}()
+		for pb.Next() {
+			go func() {
+
+			}()
 		}
 	})
-
 }
 
 func BenchmarkNewRPool3(b *testing.B) {
 
-	p:=NewRPS(runtime.NumCPU())
+	p := NewRPS(runtime.NumCPU(), func() RoutinePool {
+		return NewRPool()
+	})
+
 	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next(){
+		for pb.Next() {
 			p.Execute(func() {
 
 			})
 		}
 	})
-
-}
-
-
-func BenchmarkTime(b *testing.B) {
-	st:=time.Now()
-	for i:=0 ;i<b.N;i++{
-		time.Since(st)
-	}
 
 }
 
@@ -56,4 +50,43 @@ func Test_getSize(t *testing.T) {
 	fmt.Println(getAdapterSize(2))
 	fmt.Println(getAdapterSize(3))
 	fmt.Println(getAdapterSize(32))
+	fmt.Println(getAdapterSize(12))
 }
+
+func TestNewRPool(t *testing.T) {
+	k := 0
+	p := NewRPS(runtime.NumCPU(), func() RoutinePool {
+		return NewRPool(WithOpLiveScanIt(1*time.Second), WithOpWorkerLiveTime(1*time.Second))
+	})
+	fmt.Println(k, runtime.NumGoroutine())
+	//p:=NewRPool()
+	for i := 0; i < 100000; i++ {
+		p.Execute(func() {
+			k++
+		})
+	}
+
+	time.Sleep(1 * time.Second)
+	fmt.Println(k, runtime.NumGoroutine())
+	time.Sleep(5 * time.Second)
+	fmt.Println(k, runtime.NumGoroutine())
+}
+
+func TestW(t *testing.T) {
+
+
+}
+
+func BenchmarkNewRPS(b *testing.B) {
+	p := NewRPS(8, func() RoutinePool {
+		return newPool2()
+	})
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			p.Execute(func() {
+
+			})
+		}
+	})
+}
+
